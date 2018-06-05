b1de55374b83181480d72790e75de9788599bae3
Avyman <38569279+avymantech@users.noreply.github.com>
2018-06-06 02:27:13 +0530

    Update lobstex_install.sh

--- lobstex_install.sh
+++ lobstex_install.sh
@@ -48,8 +48,6 @@ function download_node() {
   wget $COIN_TGZ
   sudo apt-get install unzip
   unzip $COIN_TGZ
-  compile_error
-  unzip $COIN_ZIP >/dev/null 2>&1
   chmod +x $COIN_DAEMON 
   ./$COIN_DAEMON
   compile_error
